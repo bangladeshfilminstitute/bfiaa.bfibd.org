ea0e038dcdd67076e4d061fb23f35cdd4d9bbf83
Bangladesh Film Institute (BFI) <bangladesh.film.institute@gmail.com>
2026-09-12 11:39:50 +0600

    Add clean_headers.js

---
+++ clean_headers.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+
+let indexContent = fs.readFileSync('index.html', 'utf8');
+
+indexContent = indexContent.replace(
+  `<!-- ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+     FOUNDER SECTION
+■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■ -->
+<!-- ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+     BIENNIAL CONFERENCE 2019 SECTION
+■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■ -->`,
+  `<!-- ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+     BIENNIAL CONFERENCE 2019 SECTION
+■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■ -->`
+);
+
+indexContent = indexContent.replace(
+  `</section>\n\n<section class="about" id="founder"`,
+  `</section>\n\n<!-- ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■\n     FOUNDER SECTION\n■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■ -->\n<section class="about" id="founder"`
+);
+
+fs.writeFileSync('index.html', indexContent, 'utf8');
+console.log('Cleaned up comment headers in index.html');
